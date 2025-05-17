d36c960fa202b852512a3829c47a240a4dac2bee
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-05-17 06:46:18 +0530

    add to java script file

---
+++ Type speed Game/js/Script.js
@@ -0,0 +1,27 @@
+const typingText = document.querySelector(".typing-text p");
+inputField =  document.querySelector(".wrapper .input-field");
+let charIndex = 0;
+
+function randomParagraph(){
+    let randomIndex = Math.floor(Math.random() * paragraph.length);
+    paragraph[randomIndex].split("").forEach(span =>{
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+    document.addEventListener("keydown",() => inputField.focus());
+    document.addEventListener("click",() => inputField.focus());
+}
+
+function initTyping(){
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inputField.value.split("")[charIndex];
+    if(characters[charIndex].innerText === typedChar){
+       characters[charIndex].classList.add("Correct");
+    }else{
+       characters[charIndex].classList.add("Incorrect")
+    }
+    charIndex++;
+}
+
+randomParagraph();
+inputField.addEventListener("input",initTyping);
